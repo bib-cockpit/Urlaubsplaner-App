6e24b25de028b54650cd187f77d1eae4654bb50b
Peter Hornburger <p.hornburger@gmail.com>
2024-05-17 20:37:25 +0200

    Freitag 20:45

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -330,7 +330,7 @@ export class AppComponent implements OnInit, OnDestroy, AfterContentChecked {
 
         this.Pool.ShowProgress = false;
 
-        if(this.Pool.Mitarbeiterdaten.Planeradministrator === false) {
+        if(this.Pool.Mitarbeiterdaten.Planeradministrator === true) {
 
           Page = this.Pool.Appeinstellungen.AdminStartseite;
         }
